dd3197a03989bda16140fcbb55bfb2c93650620e
nayab jahan <nayabzafir@gmail.com>
2022-08-31 07:28:48 +0530

    Added:Regex Example

---
+++ src/main/java/com/blz/userRegistration/UserRegistrationMain.java
@@ -0,0 +1,7 @@
+package com.blz.userRegistration;
+
+public class UserRegistrationMain {
+    public static void main(String[] args) {
+        System.out.println("welcome to Regex Example");
+    }
+}
