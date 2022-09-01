fa1387aa8f1f164233f8acf184a7a1748981a41c
nayab jahan <nayabzafir@gmail.com>
2022-09-02 03:24:55 +0530

    Added:First Letter should be Capital

--- src/main/java/com/blz/userRegistration/UserRegistrationMain.java
+++ src/main/java/com/blz/userRegistration/UserRegistrationMain.java
@@ -1,7 +1,30 @@
 package com.blz.userRegistration;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class UserRegistrationMain {
+    private static final int VALIDATE_FIRSTNAME =1 ;
+
     public static void main(String[] args) {
         System.out.println("welcome to Regex Example");
+        UserRegistrationTest validate = new UserRegistrationTest();
+        java.util.Scanner scan = new java.util.Scanner(System.in);
+        System.out.println("1.To Validate the First name.");
+        System.out.println("Enter the option number to perform the operation");
+        int option = scan.nextInt();
+        switch (option) {
+            case VALIDATE_FIRSTNAME:
+                System.out.println("Enter the First Name. Note: first letter should be capital");
+                String firstName = scan.next();
+                validate.validateFirstName(firstName);
+                break;
+            default:
+                System.out.println("Please Enter valid option to validate the pattern");
+                break;
+        }
     }
+
 }
+
+
---
+++ src/main/java/com/blz/userRegistration/UserRegistrationTest.java
@@ -0,0 +1,10 @@
+package com.blz.userRegistration;
+
+public class UserRegistrationTest {
+    public void validateFirstName(String firstName){
+        UserInformation store =new UserInformation();
+        store.setFirstName("^[A-Z]{1}[a-z]{3,}$");
+        String first= store.getFirstName();
+        store.validatingInput(firstName,first,"FirstName");
+    }
+}
